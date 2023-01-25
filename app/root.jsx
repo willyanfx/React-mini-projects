e08cceede0f933d057112b5328535280d1f6f274
willyanfx <willyan.fx@gmail.com>
2023-01-24 21:23:44 -0800

    ErrorBoundary

--- app/root.jsx
+++ app/root.jsx
@@ -19,6 +19,31 @@ export const meta = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export function ErrorBoundary({ error }) {
+  return (
+    <html lang='en'>
+      <head>
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main className='error'>
+          <h1>An ERROR Occurred!</h1>
+          <p>{error.message}</p>
+        </main>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
 export default function App() {
   return (
     <html lang='en'>
