45482461934e46fe424cfc7a828bfdd68645de06
willyanfx <willyan.fx@gmail.com>
2023-01-24 21:17:56 -0800

    useActionData

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,12 +1,16 @@
-import { Form } from "@remix-run/react";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import styles from "./NewNote.css";
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
 function NewNote() {
+  const navigation = useNavigation();
+  const data = useActionData();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Form method='post' id='note-form'>
+      {data?.message && <p>{data.message}</p>}
       <p>
         <label htmlFor='title'>Title</label>
         <input type='text' id='title' name='title' required />
@@ -16,7 +20,9 @@ function NewNote() {
         <textarea id='content' name='content' rows='5' required />
       </p>
       <div className='form-actions'>
-        <button>Add Note</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Adding..." : "Add Note"}
+        </button>
       </div>
     </Form>
   );
