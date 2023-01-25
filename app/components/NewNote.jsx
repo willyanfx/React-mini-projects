9f666b4eb3ed07d499f4bb28177ea07f09d89cfd
willyanfx <willyan.fx@gmail.com>
2023-01-24 20:53:37 -0800

    componsnts

---
+++ app/components/NewNote.jsx
@@ -0,0 +1,25 @@
+import { Form } from "@remix-run/react";
+import styles from "./NewNote.css";
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+function NewNote() {
+  return (
+    <form method='post' id='note-form'>
+      <p>
+        <label htmlFor='title'>Title</label>
+        <input type='text' id='title' name='title' required />
+      </p>
+      <p>
+        <label htmlFor='content'>Content</label>
+        <textarea id='content' name='content' rows='5' required />
+      </p>
+      <div className='form-actions'>
+        <button>Add Note</button>
+      </div>
+    </form>
+  );
+}
+
+export default NewNote;
