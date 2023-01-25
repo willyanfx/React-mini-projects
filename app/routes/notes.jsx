cf295f62b37f628c0d39c42339a095f7c033403a
willyanfx <willyan.fx@gmail.com>
2023-01-24 20:55:02 -0800

    added fn action and loader

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,10 +1,36 @@
-import { Link } from "@remix-run/react";
+import { json, redirect } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import NewNote, { links as newNotesStyles } from "~/components/NewNote";
+import NoteList, { links as noteListStyles } from "~/components/NoteList";
+import { getStoredNotes, storeNotes } from "~/data/notes";
+
+export function links() {
+  return [...newNotesStyles(), ...noteListStyles()];
+}
+
+export async function action({ request }) {
+  const form = await request.formData();
+  const noteData = Object.fromEntries(form);
+  console.log(noteData);
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+
+  return redirect("/notes");
+}
+
+export async function loader() {
+  const notes = await getStoredNotes();
+  return notes;
+}
 
 export default function NotesPage() {
+  const notes = useLoaderData();
   return (
     <main>
-      <h1>My notes</h1>
-      <p>Try our warly beta and never loose track of your notes again!</p>
+      <NewNote />
+      <NoteList notes={notes} />
       <p id='cta'>
         <Link to='/'>back</Link>
       </p>
