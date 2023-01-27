240fe67aa48be6c01d6c29b33407cf3b32014f2c
willyanfx <willyan.fx@gmail.com>
2023-01-26 17:39:32 -0800

    added Navbar

--- app/root.jsx
+++ app/root.jsx
@@ -7,8 +7,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import styles from "~/styles/main.css";
-import MainNavigation from "./components/MainNavigation";
+import styles from "~/styles/shared.css";
+import MainHeader from "./components/navigation/MainHeader";
+
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
@@ -19,41 +20,26 @@ export const meta = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export function ErrorBoundary({ error }) {
-  return (
-    <html lang='en'>
-      <head>
-        <Meta />
-        <Links />
-        <title>An error occurred!</title>
-      </head>
-      <body>
-        <header>
-          <MainNavigation />
-        </header>
-        <main className='error'>
-          <h1>An ERROR Occurred!</h1>
-          <p>{error.message}</p>
-        </main>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
-
 export default function App() {
   return (
     <html lang='en'>
       <head>
         <Meta />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
         <Links />
       </head>
       <body>
         <header>
-          <MainNavigation />
+          <MainHeader />
         </header>
         <Outlet />
         <ScrollRestoration />
