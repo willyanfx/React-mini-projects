ab3de17bc3d07420cfec04cd2c10e7e7b3be2d7e
willyanfx <willyan.fx@gmail.com>
2023-01-31 20:14:40 -0800

    start auth

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,16 +1,21 @@
-import { Link, useSearchParams } from "@remix-run/react";
+import { Form, Link, useNavigation, useSearchParams } from "@remix-run/react";
 import { FaLock, FaUserPlus } from "react-icons/fa";
 
 function AuthForm() {
   const [searchParams] = useSearchParams();
+
+  const navigation = useNavigation();
+
   const authMode = searchParams.get("mode") || "login";
 
   const submitBtnCaption = authMode === "login" ? "Login" : "Create User";
   const toggleBtnCaption =
     authMode === "login" ? "Create a new user" : "Login with existent user";
 
+  const isSubmitting = navigation.state !== "idle";
+
   return (
-    <form method='post' className='form' id='auth-form'>
+    <Form method='post' className='form' id='auth-form'>
       <div className='icon-img'>
         {authMode === "login" ? <FaLock /> : <FaUserPlus />}
       </div>
@@ -23,12 +28,14 @@ function AuthForm() {
         <input type='password' id='password' name='password' minLength={7} />
       </p>
       <div className='form-actions'>
-        <button>{submitBtnCaption}</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Authentication ..." : submitBtnCaption}
+        </button>
         <Link to={authMode === "login" ? "?mode=signup" : "?mode=login"}>
           {toggleBtnCaption}
         </Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -4,6 +4,18 @@ export function links() {
   return [{ rel: "stylesheet", href: authStyles }];
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get("mode") || "login";
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  if (authMode === "login") {
+  } else {
+  }
+}
+
 export default function AuthPage() {
   return (
     <>
