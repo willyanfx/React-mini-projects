470baa2faec15ba848464dc2c1ade00dbb9d7d4a
willyanfx <willyan.fx@gmail.com>
2023-01-31 17:33:12 -0800

    edit expenses

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -31,3 +31,19 @@ export async function getExpense(id) {
     throw error;
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    return await prisma.expense.update({
+      where: { id: id },
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
--- app/routes/__app/expenses/$id.jsx
+++ app/routes/__app/expenses/$id.jsx
@@ -1,6 +1,25 @@
-import { useLoaderData, useNavigate } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { updateExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  console.log(expenseData, expenseData);
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect("/expenses");
+}
 
 export default function UpdatePage() {
   const navigate = useNavigate();
