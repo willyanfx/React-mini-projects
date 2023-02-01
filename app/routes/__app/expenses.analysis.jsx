604a6a0dfa4105d42e25df5008b65b91c59c264f
willyanfx <willyan.fx@gmail.com>
2023-01-31 21:34:58 -0800

    use db data for the graphics

--- app/routes/__app/expenses.analysis.jsx
+++ app/routes/__app/expenses.analysis.jsx
@@ -1,16 +1,45 @@
 import Chart from "~/components/expenses/Chart";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import { getExpenses } from "../../data/expenses.server";
+import { json } from "@remix-run/node";
+import Error from "~/components/util/Error";
+import { Link, Outlet, useCatch, useLoaderData } from "@remix-run/react";
 
-const DUMMY_EXPENSES = [
-  { id: "e1", title: "First", amount: 12.99, date: new Date().toISOString() },
-  { id: "e2", title: "Second", amount: 12.99, date: new Date().toISOString() },
-];
+export async function loader() {
+  const expenses = await getExpenses();
 
-export default function AnalysisPage() {
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      { message: "could not load expenses" },
+      {
+        status: 404,
+        statusText: "Expenses not found",
+      }
+    );
+  }
+
+  return expenses;
+}
+
+export function CatchBoundary() {
+  const response = useCatch();
   return (
     <main>
-      <Chart expenses={DUMMY_EXPENSES} />
-      <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+      ❗️❗️❗️❗️
+      <Error title={response.statusText}>
+        <p>{response.data?.message}</p>
+      </Error>
+    </main>
+  );
+}
+
+export default function AnalysisPage() {
+  const data = useLoaderData();
+
+  return (
+    <main>
+      <Chart expenses={data} />
+      <ExpenseStatistics expenses={data} />
     </main>
   );
 }
