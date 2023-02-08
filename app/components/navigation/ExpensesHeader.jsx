4dc968ce6b52793666a46a8a35f8f4d9a594087b
willyanfx <willyan.fx@gmail.com>
2023-02-07 21:26:40 -0800

    form logout

--- app/components/navigation/ExpensesHeader.jsx
+++ app/components/navigation/ExpensesHeader.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "@remix-run/react";
+import { NavLink, Form } from "@remix-run/react";
 
 import Logo from "../util/Logo";
 
@@ -19,7 +19,9 @@ function ExpensesHeader() {
         </ul>
       </nav>
       <nav id='cta-nav'>
-        <button className='cta'>Logout</button>
+        <Form method='post' action='/logout' id='logout-form'>
+          <button className='cta'>Logout</button>
+        </Form>
       </nav>
     </header>
   );
