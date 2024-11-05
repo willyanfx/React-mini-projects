b9d39c01f0b9b753a828342586a3468a82898985
willyanfx <willyan.fx@gmail.com>
2024-11-04 20:34:39 -0700

    svelte

---
+++ svelte-basic/src/components/header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	// let name: string = $state('scott');
+	// let lastname: string = $state('sky');
+	// let status: 'OPEN' | 'CLOSE' = $state('OPEN');
+	// let full_name = $derived(`${name} ${lastname}`);
+	let { header }: { header?: string } = $props();
+	let background = header ? 'back' : 'pink';
+	function onclick() {
+		status = status === 'OPEN' ? 'CLOSE' : 'OPEN';
+	}
+</script>
+
+<header>
+	<h1>
+		{header ? header : 'User'}'s Form
+	</h1>
+</header>
+
+<!-- <input type="text" bind:value={name} />
+<input type="text" bind:value={lastname} />
+<p>the store is now {status}</p>
+<!-- <button onclick={toggle}>{status === 'OPEN' ? 'CLOSE' : 'OPEN'}</button> -->
+<!-- <button {onclick}>{status === 'OPEN' ? 'CLOSE' : 'OPEN'}</button> -->
+
+<style>
+	header {
+		background: red;
+	}
+</style>
