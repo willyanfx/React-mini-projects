f65765a5785151c372a2122bc45d97c471ea374c
willyanfx <willyan.fx@gmail.com>
2024-11-05 21:06:51 -0700

    components

---
+++ job-list-vue/src/components/Card.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps({
+  bg: {
+    type: String,
+    default: 'bg-gray-100',
+  },
+})
+</script>
+
+<template>
+  <div :class="`p-6 rounded-lg shadow-md ${bg}`">
+    <slot></slot>
+  </div>
+</template>
---
+++ job-list-vue/src/components/JobListings.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import jobData from '@/jobs.json'
+import { ref } from 'vue'
+import JobListening from './JobListing.vue'
+
+const jobs = ref(jobData)
+</script>
+
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 text-center">Browse Jobs</h2>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListening v-for="job in jobs" :key="job.id" :job="job" />
+      </div>
+    </div>
+  </section>
+</template>
