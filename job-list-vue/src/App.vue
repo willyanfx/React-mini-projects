2a5d4467f86cc23bc200332863690df078039de9
willyanfx <willyan.fx@gmail.com>
2024-11-05 21:07:00 -0700

    kick start

---
+++ job-list-vue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ job-list-vue/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeroBanner from '@/components/HeroBanner.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+</script>
+
+<template>
+  <main>
+    <HeroBanner />
+    <HomeCards />
+    <JobListings />
+  </main>
+</template>
